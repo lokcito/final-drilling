0e35b4db972e55a4ff5e8366769c5420b5d429d6
ray <enciso.leo@gmail.com>
2023-08-18 01:22:48 +0000

    Implementacion de admin.py

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -3,6 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import Laboratorio, DirectorGeneral, Producto
 
-admin.site.register(Laboratorio)
-admin.site.register(DirectorGeneral)
-admin.site.register(Producto)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('nombre', )
+
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 
+        'f_fabricacion_year', 'p_costo', 'p_venta')
+    
+    ordering = ('nombre', 'laboratorio')
+
+    list_display_links = ('nombre', 'laboratorio')
+
+    list_filter = ('nombre', 'laboratorio')
+
+    def f_fabricacion_year(self, obj):
+        return obj.f_fabricacion.year
+    
+    f_fabricacion_year.short_description = "F Fabricacion"
+
+admin.site.register(Producto, ProductoAdmin)
--- laboratorio/models.py
+++ laboratorio/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length = 128)
 
+    def __str__(self):
+        return self.nombre
+
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length = 128)
     laboratorio = models.OneToOneField(Laboratorio, on_delete = models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     nombre = models.CharField(max_length = 128)
@@ -14,3 +20,6 @@ class Producto(models.Model):
     f_fabricacion = models.DateField()
     p_costo = models.DecimalField(max_digits = 12, decimal_places = 2)
     p_venta = models.DecimalField(max_digits = 12, decimal_places = 2)
+
+    def __str__(self):
+        return self.nombre
