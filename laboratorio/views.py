ac355f86354c1a0a2e5255b854f175d61ca240de
ray <enciso.leo@gmail.com>
2023-08-19 16:17:28 +0000

    incluyendo method post

--- laboratorio/views.py
+++ laboratorio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import LaboratorioForm
 from .models import Laboratorio
+from django.http import HttpResponseRedirect
 # Create your views here.
 def v_list(request):
     context = {
@@ -9,6 +10,13 @@ def v_list(request):
     return render(request, 'list.html', context)
 
 def v_create(request):
+    if request.method == 'POST':
+        datos = request.POST.copy()
+        formcrear = LaboratorioForm(datos)
+        if formcrear.is_valid():
+            formcrear.save()
+            return HttpResponseRedirect("/")
+
     context = {
         'formulario': LaboratorioForm()
     }
@@ -18,7 +26,11 @@ def v_update(request, laboratorio_id):
     lab = Laboratorio.objects.get(id = laboratorio_id)
 
     if request.method == 'POST':
-        pass # Aqui incluir mas codigo
+        datos = request.POST.copy()
+        formeditar = LaboratorioForm(datos, instance = lab)
+        if formeditar.is_valid():
+            formeditar.save()
+        return HttpResponseRedirect("/")
     else:
         context = {
             'formedicion': LaboratorioForm(instance = lab)
@@ -26,5 +38,15 @@ def v_update(request, laboratorio_id):
         return render(request, 'update.html', context)
 
 def v_delete(request, laboratorio_id):
-    context = {}
+    if request.method == "POST":
+        from .models import Producto, DirectorGeneral
+        Producto.objects.filter(laboratorio = laboratorio_id).delete()
+        DirectorGeneral.objects.filter(laboratorio = laboratorio_id).delete()
+
+        Laboratorio.objects.get(id = laboratorio_id).delete()
+        return HttpResponseRedirect("/")
+
+    context = {
+        'lab': Laboratorio.objects.get(id = laboratorio_id)
+    }
     return render(request, 'delete.html', context)
