2d337b163755ca8cf935f42dae73d1a1692dc981
ray <enciso.leo@gmail.com>
2023-08-19 15:10:46 +0000

    creacion formularios

--- laboratorio/views.py
+++ laboratorio/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-
+from .forms import LaboratorioForm
+from .models import Laboratorio
 # Create your views here.
 def v_list(request):
-    context = {}
+    context = {
+        'labos': Laboratorio.objects.all()
+    }
     return render(request, 'list.html', context)
 
 def v_create(request):
-    context = {}
+    context = {
+        'formulario': LaboratorioForm()
+    }
     return render(request, 'create.html', context)
 
 def v_update(request, laboratorio_id):
-    context = {}
-    return render(request, 'update.html', context)
+    lab = Laboratorio.objects.get(id = laboratorio_id)
+
+    if request.method == 'POST':
+        pass # Aqui incluir mas codigo
+    else:
+        context = {
+            'formedicion': LaboratorioForm(instance = lab)
+        }
+        return render(request, 'update.html', context)
 
 def v_delete(request, laboratorio_id):
     context = {}
